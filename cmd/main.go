844124f18a1f100d5a27d5230ba158d98689aa8b
Anna <anna95ml@gmail.com>
2026-05-04 19:46:57 +0500

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"log"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
 func main() {
+	//создаем логгер
+	logger := log.Default()
+	//fmt.Println("Создали логгер")
+
+	srv := server.NewServer(logger)
+	//fmt.Println("Создали сервер из логгера")
+
+	//fmt.Println("Запускаем сервер")
+	logger.Printf("Запуск сервера %s", srv.HttpSrv.Addr)
+	if err := srv.HttpSrv.ListenAndServe(); err != nil {
+		logger.Fatalf("Ошибка сервера: %v", err)
+		//srv.Lgr.Fatal(err)
+	}
 
 }
